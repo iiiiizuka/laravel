a93f0757c19b6f5b44862f923f8b3661ec624b58
iiiiizuka <meshiiiiiiiiii@gmail.com>
2024-02-05 22:13:54 +0900

    add file

---
+++ app/ValueObjects/EmailValueObject.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\ValueObjects;
+
+class EmailValueObject
+{
+    private $value;
+
+    public function __construct(string $value)
+    {
+        if (empty($value) || !filter_var($value, FILTER_VALIDATE_EMAIL)) {
+            throw new \InvalidArgumentException('引数が不正です。');
+        }
+
+        $this->value = $value;
+    }
+
+    public function get(): string
+    {
+        return $this->value;
+    }
+
+    public static function validate(): array
+    {
+        return ['required', 'string', 'email'];
+    }
+}
