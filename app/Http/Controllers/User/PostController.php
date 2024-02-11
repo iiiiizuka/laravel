befe2433c8c641a3226e8930bbcb3681c6836e69
iiiiizuka <meshiiiiiiiiii@gmail.com>
2024-02-11 22:28:26 +0900

    add controller

---
+++ app/Http/Controllers/User/PostController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\UseCases\User\Post\CreateAction;
+use App\UseCases\User\Post\DestroyAction;
+use App\UseCases\User\Post\IndexAction;
+use App\UseCases\User\Post\StoreAction;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function __construct(
+        private IndexAction $index_action,
+        private CreateAction $create_action,
+        private StoreAction $store_action,
+        private DestroyAction $destroy_action,
+    ) { }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
