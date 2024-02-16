7367e58f05312fbb25d16f758e13c4bad03aac7b
iiiiizuka <meshiiiiiiiiii@gmail.com>
2024-02-16 22:57:14 +0900

    add column

--- database/migrations/2024_02_11_130646_create_posts_table.php
+++ database/migrations/2024_02_11_130646_create_posts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
+            $table->unsignedInteger('likes');
             $table->text('content');
             $table->timestamps();
         });
