23895964360cc5273bdf5ca66f532a2c561e4328
iiiiizuka <meshiiiiiiiiii@gmail.com>
2024-02-18 23:21:03 +0900

    update files

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,17 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
     use HasFactory;
 
-    public function posts()
-    {
-        return $this->belongsToMany(Post::class);
-    }
-
     protected $fillable = [
         'name',
     ];
+
+    public function posts(): BelongsToMany
+    {
+        return $this->belongsToMany(Post::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -45,4 +46,9 @@ class User extends Authenticatable
         'password' => 'hashed',
         'birth_on' => BirthOn::class,
     ];
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
 }
