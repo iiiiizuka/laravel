a032b93fc8390b608066231e2eedf50129761c8c
iiiiizuka <meshiiiiiiiiii@gmail.com>
2024-02-25 21:46:21 +0900

    update file

--- app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Admin\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
