f548a6dcf35ce82f39083b4551d59f364eafc206
iiiiizuka <meshiiiiiiiiii@gmail.com>
2024-04-02 22:40:25 +0900

    add files

---
+++ resources/views/admin/tag/index.blade.php
@@ -0,0 +1,11 @@
+<x-admin.app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <h1>Tags</h1>
+
+            @foreach ($tags as $tag)
+                <p>{{ $tag->name }}</p>
+            @endforeach
+        </div>
+    </div>
+</x-admin.app-layout>
